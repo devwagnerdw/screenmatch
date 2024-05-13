3e7a96d04ea92f7a1c104c16f1c251089b7ffa58
Vagne alves <vagnerdw1@gmail.com>
2024-05-13 11:22:46 -0300

    Enhancement: Optimizing Collection Manipulation

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,10 +1,7 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporadas;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,27 +18,12 @@ public class ScreenmatchApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json= consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6002159a");
-		ConverteDados conversor= new ConverteDados();
-		DadosSerie dados= conversor.obterDados(json,DadosSerie.class);
-		System.out.println(dados);
+		Principal principal= new Principal();
 
-		json=consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6002159a");
-		DadosEpisodio dadosEpisodio= conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
+		principal.exibirMenu();
 
-
-		List<DadosTemporadas> temporadas= new ArrayList<>();
-		for (int i=1; i<=dados.totalTemporadas(); i++){
-			json = consumoApi.obterDados ( "https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6002159a");
-			DadosTemporadas dadosTemporadas= conversor.obterDados(json,DadosTemporadas.class);
-
-			temporadas.add(dadosTemporadas);
-
-		}
-		temporadas.forEach(System.out::println);
 
 
 	}
+
 }
--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,2 +1,54 @@
-package br.com.alura.screenmatch.principal;public class Principal {
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporadas;
+import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura= new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+
+     private ConverteDados conversor= new ConverteDados();
+
+    private final String ENDERECO="https://www.omdbapi.com/?t=";
+    private final String API_KEY="&apikey=6002159a";
+    public void  exibirMenu(){
+        System.out.println("Digite o nome da série para a busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ","+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+
+        List<DadosTemporadas> temporadas= new ArrayList<>();
+		for (int i=1; i<=dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&season=" + i + API_KEY);
+            DadosTemporadas dadosTemporadas= conversor.obterDados(json,DadosTemporadas.class);
+
+			temporadas.add(dadosTemporadas);
+
+		}
+		temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporadas = temporadas.get(i).episodios();
+//
+//            for (int j = 0; j < episodiosTemporadas.size(); j++) {
+//                System.out.println(episodiosTemporadas.get(j).titulo());
+//
+//            }
+//            System.out.println(episodiosTemporadas.size());
+//        }
+
+
+
+        temporadas.forEach(t->t.episodios().forEach(e -> System.out.println(e.titulo())));
+        temporadas.forEach(System.out::println);
+    }
 }
